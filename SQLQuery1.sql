07700c99171fc99e99868a783440603bd8b10d39
israel mvono <israelmvono@gmail.com>
2025-02-26 11:40:32 +0300

    Add SQL script for creating database and pin table

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+create database atm
+create table pin(pin_no int ,acc_no int)
+insert into pin values(1234,55555)
+insert into pin values(5678,66666)
+insert into pin values(3456,00000)
+insert into pin values(6789,11111)
+Select * from pin
